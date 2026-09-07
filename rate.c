c5aced9df9221f02656ad8494d74c59f97493138
carraly <vcarraly@gmail.com>
2026-09-07 20:24:38 -0300

    Corrige os erros do meu estado de exaustão e adiciona a verificação para as tasks serem salvas na fila na ordem correta

--- rate.c
+++ rate.c
@@ -1,7 +1,6 @@
 #include "header.h"
 
-void rate_add_tasks_queue(Node** tasks_queue, Node* temp) {
-    Task new_task = temp->task;
+void rate_add_tasks_queue(Node** tasks_queue, Task new_task) {
 
     if (*tasks_queue == NULL) {
         *tasks_queue = (Node*) malloc(sizeof(Node));
@@ -10,13 +9,32 @@ void rate_add_tasks_queue(Node** tasks_queue, Node* temp) {
     }
 
     Node* last = *tasks_queue;
+    if (new_task.time_until_new < last->task.time_until_new) {
+        *tasks_queue = (Node*) malloc(sizeof(Node));
+        (*tasks_queue)->task = new_task;
+        (*tasks_queue)->next = last;
+        return;
+        }
+
     while (last->next != NULL) {
+        if (new_task.time_until_new < last->next->task.time_until_new) {
+            break;
+        }
         last = last->next;
     }
-    last->next = (Node*) malloc(sizeof(Node));
-    last = last->next;
-    last->task = new_task;
-    last->next = NULL;
+
+    if (last->next == NULL) {
+        last->next = (Node*) malloc(sizeof(Node));
+        last = last->next;
+        last->task = new_task;
+        last->next = NULL;
+    }else {
+        Node* temp = last->next;
+        last->next = (Node*) malloc(sizeof(Node));
+        last = last->next;
+        last->task = new_task;
+        last->next = temp;
+    }
 }
 
 void rate_scheduler(Node* head, long total_time) {
@@ -27,12 +45,15 @@ void rate_scheduler(Node* head, long total_time) {
     Node* temp = head;
 
     while (passed_time < total_time) {
+        temp = head;
         while (temp != NULL) {
-            if (passed_time == 0 || temp->task.time_until_new / passed_time == 0) {
-                rate_add_tasks_queue(&tasks_queue, temp);
+            if (passed_time == 0 || passed_time % temp->task.time_until_new == 0) {
+                rate_add_tasks_queue(&tasks_queue, temp->task);
             }
             temp = temp->next;
         }
         passed_time++;
     }
+
+    fclose(file);
 }
